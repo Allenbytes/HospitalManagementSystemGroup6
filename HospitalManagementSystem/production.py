b34862700ec26ca4ad9315bd38291ae9a5759ee4
Allenbytes <lzcunanan1@student.hau.edu.ph>
2024-10-21 00:59:49 +0800

    new files

---
+++ HospitalManagementSystem/production.py
@@ -0,0 +1,17 @@
+import os
+from .settings import *  # Import all settings from base settings
+
+DEBUG = False
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
